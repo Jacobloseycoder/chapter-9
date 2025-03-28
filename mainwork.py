58991867e6493d223aa2b282c956f6a7075e24bd
gageb438 <gageb534@gmail.com>
2025-03-28 13:16:18 -0500

    Update mainwork.py

--- mainwork.py
+++ mainwork.py
@@ -1,20 +1,27 @@
 def main():
-  #main should call all other functions to build the entire program
+    #main should call all other functions to build the entire program
+    pass
 def get_shift():
-  #get_shift should prompt the user for the shift value and return the value as a string 
-  #Validate for integers 1-25, inclusive
+    #get_shift should prompt the user for the shift value and return the value as a string 
+    #Validate for integers 1-25, inclusive
+    pass
 def choose_option():
-  #choose_option should prompt the user to choose to encode or decode. 
-  #It should return True if the user chooses encode and False if the user chooses decode.
-  #Validate to only accept the appropriate prompts for encode and decode.
+    #choose_option should prompt the user to choose to encode or decode. 
+    #It should return True if the user chooses encode and False if the user chooses decode.
+    #Validate to only accept the appropriate prompts for encode and decode.
+    pass
 def get_message():
-  #get_message should prompt the user to enter a message to encode or decode.  It should return that message
-def create_key():
-  #create_key should accept the shift value from get_shift.
-  #It should create the caesar cipher according to the shift value and store the key in a dictionary and return the dictionary as the key
-def encode():
-  #encode should accept message as a string and key as a dictionary.
-  #It should encode the message using the key and return the encoded message as a string
-def decode():
-  #decode should accept message as a string and key as a dictionary.
-  #It should decode the message using the key and return the decoded message as a string
+    #get_message should prompt the user to enter a message to encode or decode.  It should return that message
+    pass
+def create_key(shift):
+    #create_key should accept the shift value from get_shift.
+    #It should create the caesar cipher according to the shift value and store the key in a dictionary and return the dictionary as the key
+    pass
+def encode(message, key):
+    #encode should accept message as a string and key as a dictionary.
+    #It should encode the message using the key and return the encoded message as a string
+    pass
+def decode(message, key):
+    #decode should accept message as a string and key as a dictionary.
+    #It should decode the message using the key and return the decoded message as a string
+    pass
