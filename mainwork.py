47297fe0908d8296898319d9538fc9dff5a51631
jacob losey <loseyjacob1@gmail.com>
2025-04-02 13:11:30 -0500

    Update mainwork.py
    Signed-off-by: jacob losey <loseyjacob1@gmail.com>

--- mainwork.py
+++ mainwork.py
@@ -1,10 +1,11 @@
 def main():
     #main should call all other functions to build the entire program
-    #main should call all other functions to build the entire program
+    #calls all other functions
     shift = get_shift()
     message = get_message()
     chooce = choose_option()
     key = create_key(shift)
+    #finds if neds to encode or decode
     if chooce == True:
         print("Here is your encoded message: ")
         message = encode(message, key)
@@ -13,24 +14,30 @@ def main():
         print("Here is your decoded message: ")
         message = decode(message, key)
         print(message)
+        
 def get_shift():
     #get_shift should prompt the user for the shift value and return the value as a string 
     #Validate for integers 1-25, inclusive
     shift = int(input('enter how many you want to shift'))
+    #makes sure not more or less then alowed
     while shift < 0 or shift > 26:
         shift = int(input('enter how many you want to shift'))
     return shift
+
 def choose_option():
     #choose_option should prompt the user to choose to encode or decode. 
     #It should return True if the user chooses encode and False if the user chooses decode.
     #Validate to only accept the appropriate prompts for encode and decode.
     chooce = input('do you want to encode or decode: ')
+    #make sure you only choose en or decode
     while chooce != 'encode' and chooce != 'decode':
         chooce = input('please only select encode or decode')
     if chooce == 'encode':
+        #returns encode or decode
         return True
     else:
         return False
+    
 def get_message():
     #get_message should prompt the user to enter a message to encode or decode.  It should return that message
     
@@ -49,8 +56,8 @@ def get_message():
     return message
 
 def create_key(shift):
-    #create_key should accept the shift value from get_shift.
-    #It should create the caesar cipher according to the shift value and store the key in a dictionary and return the dictionary as the key
+    # create_key should accept the shift value from get_shift.
+    # It should create the caesar cipher according to the shift value and store the key in a dictionary and return the dictionary as the key
     
     # import string
     import string
@@ -73,51 +80,81 @@ def create_key(shift):
         old_letter = letter_to_num[letter]
         old_letter += shift
         
-        # translate if the old letter goes over z
+        # translate if the old letter goes over 'z'
         while old_letter >= 26:
             old_letter = old_letter - 26
     
         old_letter = num_to_letter[old_letter]
         
         # add to key
-        key[old_letter] = letter
+        key[letter] = old_letter  # This now stores the encoding mapping
     
     # return the key
     return key
-  
+
 def encode(message, key):
-    #encode should accept message as a string and key as a dictionary.
-    #It should encode the message using the key and return the encoded message as a string
-    pass
+    new_message = ""
+    
+    # loop through each letter in the message
+    for letter in message:
+        # check if it's not a space, otherwise just add space to the new message
+        if letter != " ":
+            # check if it's uppercase
+            if letter.isupper():
+                capital = True
+                # convert to lowercase for translation
+                letter = letter.lower()
+            else:
+                capital = False
+            
+            # get the encoded letter from the key
+            new_letter = key[letter]
+            
+            # if the original letter was uppercase, convert the new letter to uppercase
+            if capital:
+                new_letter = new_letter.upper()
+            
+            # append the new letter to the result
+            new_message += new_letter
+        else:
+            # if it's a space, just add a space to the encoded message
+            new_message += " "
+    
+    # return the final encoded message
+    return new_message
+
 def decode(message, key):
-    #decode should accept message as a string and key as a dictionary.
-    #It should decode the message using the key and return the decoded message as a string
+    # decode should accept message as a string and key as a dictionary.
+    # It should decode the message using the key and return the decoded message as a string
+    
+    # Create the reverse key for decoding (this will map encoded letters back to the original letters)
+    reverse_key = {v: k for k, v in key.items()}
     
     # initialize variables
     new_message = ""
+    capital = False
     
     # read each letter and convert it
     for letter in message:
-        # go if it isnt a space, if it is add a space.
         if letter != " ":
-            # check if its uppercase
-            if letter.isupper() == True:
+            # check if it's uppercase
+            if letter.isupper():
                 capital = True
-                # set it to lower to translate
-                letter = letter.lower()
+                letter = letter.lower()  # convert to lowercase for decoding
                 
-            # set the new letter
-            new_letter = key[letter]
+            # set the new letter using the reverse key
+            new_letter = reverse_key[letter]
             
-            # if it was uppercase, change it to uppercase and reset capital variable
-            if capital == True:
+            # if it was uppercase, change it to uppercase and reset the capital variable
+            if capital:
                 new_letter = new_letter.upper()
                 capital = False
+                
             # add the new letter to the new message
             new_message += new_letter
         else:
-            # add the new message with a space
+            # if it's a space, just add a space to the decoded message
             new_message += " "
     
-    # return the new message
+    # print or return the new message
     return new_message
